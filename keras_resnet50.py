559fb306d0cd4421798fc1c72c3c4a334ef5554f
Jack <sjkai1@126.com>
2017-09-01 21:22:08 +0800

    Initial commit

---
+++ keras_resnet50.py
@@ -0,0 +1,18 @@
+#!
+# Reference: http://blog.csdn.net/wangli0519/article/details/72954155
+
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np 
+
+model = ResNet50(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+
+img_path = "elephant.jpg"
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+
